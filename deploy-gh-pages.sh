293903547cefe206a139727b20a709dbecaf1448
bcarsouw <bcarsouw@ilionx.com>
2017-03-09 23:41:31 +0100

    moved back stuff

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npm install
+grunt
+mkdir ../2048-build/
+cp -r dist/* ../2048-build/
+git checkout gh-pages
+rm -rf *
+cp -r ../2048-build/* .
+git add .
+git commit -m "gh-pages update"
+git push
+rm -rf ../2048-build
+git checkout master
+git checkout .
+npm install
+grunt
